9686665b61046eb9ec8d858ce83ef42d26adafbf
janlin002 <123davidbill@gmail.com>
2022-04-14 22:23:22 +0800

    react.lazy react.suspence test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { IntlProvider } from 'react-intl'
-import Formik from './Demo/Formik-Yup/Formik'
-import ReactTable from './Demo/Recat-table/ReactTable'
-import FormikYup from './Demo/Formik-Yup/FormikYup.jsx'
+
+const Formik = lazy(()=>import(/* webpackChunkName: "Formik" */'./Demo/Formik-Yup/Formik'))
+const ReactTable = lazy(()=>import(/* webpackChunkName: "ReactTable" */'./Demo/Recat-table/ReactTable'))
+const FormikYup = lazy(()=>import(/* webpackChunkName: "FormikYup" */'./Demo/Formik-Yup/FormikYup.jsx'))
+// import Formik from './Demo/Formik-Yup/Formik'
+// import ReactTable from './Demo/Recat-table/ReactTable'
+// import FormikYup from './Demo/Formik-Yup/FormikYup.jsx'
 import Header from './Header'
 import ReactIntl from './Demo/React-intl/react-intl'
 import { useSelector } from 'react-redux'
@@ -60,49 +64,51 @@ function App() {
       <IntlProvider messages={locale}>
         <Router>
           <Header />
-          <Switch>
-            <Route path='/formik' exact component={Formik}></Route>
-            <Route path='/formikyup' exact component={FormikYup}></Route>
-            <Route path='/reacttable' exact component={ReactTable}></Route>
-            <Route path='/reactintl' exact component={ReactIntl}></Route>
-            <Route path='/chart' exact component={Chart}></Route>
-            <Route path='/colspan' exact component={Colspan}></Route>
-            <Route path='/saga' exact component={Saga}></Route>
-            <Route path='/jsx-control' exact component={JsxControl}></Route>
-            <Route path='/day-js' exact component={DayJs}></Route>
-            <Route path='/formik-2' exact component={Formik2}></Route>
-            <Route path='/formik-20' exact component={Formik20}></Route>
-            <Route path='/formik-next' exact component={FormikResult}></Route>
-            <Route path='/react-modal' exact component={ReactModal}></Route>
-            <Route path='/selector-test' exact component={SelectorTest}></Route>
-            <Route path='/react-dnd' exact component={ReactDnd}></Route>
-            <Route path='/rt-job' exact component={ReactTableJob}></Route>
-            <Route path='/js-saga' exact component={JobStyleSaga}></Route>
-            <Route path='/svg' exact component={SVG}></Route>
-            <Route path='/react-slick' exact component={ReactSlick}></Route>
-            <Route path='/qrcode' exact component={QrCode}></Route>
-            <Route path='/print' exact component={Print}></Route>
-            <Route path='/print-modal' exact component={PrintModal}></Route>
-            <Route path='/react-table-pagination' exact component={ReactTablePagination}></Route>
-            <Route path='/hooks/useState' exact component={UseState}></Route>
-            <Route path='/hooks/useRef' exact component={Ref}></Route>
-            <Route path='/hooks/useEffect/Demo1' exact component={UseEffectDemo1}></Route>
-            <Route path='/hooks/useEffect/Demo2' exact component={UseEffectDemo2}></Route>
-            <Route path='/formik-submit' exact component={FromikSubmit}></Route>
-            <Route path='/interview' exact component={Interview}></Route>
-            <Route path='/bs-col-table' exact component={BsColTable}></Route>
-            <Route path='/hooks/useMemo' exact component={UseMemo}></Route>
-            <Route path='/scroll-timeline' exact component={scrollTimeline}></Route>
-            <Route path='/mixBlendMode' exact component={mixBlendMode}></Route>
-            <Route path='/clip-path' exact component={ClipPath}></Route>
-            <Route path='/custom-hook' exact component={CustomHook}></Route>
-            <Route path='/bind-js' exact component={JsBind}></Route>
-            <Route path='/theme-provider' exact component={ThemeProvider}></Route>
-            <Route path='/form-test' exact component={FormTest}></Route>
-            <Route path='/phone-charging' exact component={PhoneCharging}></Route>
-            <Route path='/react-hook-form' exact component={ReactHookForm}></Route>
-            <Route path='/ref' exact component={theRef}></Route>
-          </Switch>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Switch>
+              <Route path='/formik' exact component={Formik}></Route>
+              <Route path='/formikyup' exact component={FormikYup}></Route>
+              <Route path='/reacttable' exact component={ReactTable}></Route>
+              <Route path='/reactintl' exact component={ReactIntl}></Route>
+              <Route path='/chart' exact component={Chart}></Route>
+              <Route path='/colspan' exact component={Colspan}></Route>
+              <Route path='/saga' exact component={Saga}></Route>
+              <Route path='/jsx-control' exact component={JsxControl}></Route>
+              <Route path='/day-js' exact component={DayJs}></Route>
+              <Route path='/formik-2' exact component={Formik2}></Route>
+              <Route path='/formik-20' exact component={Formik20}></Route>
+              <Route path='/formik-next' exact component={FormikResult}></Route>
+              <Route path='/react-modal' exact component={ReactModal}></Route>
+              <Route path='/selector-test' exact component={SelectorTest}></Route>
+              <Route path='/react-dnd' exact component={ReactDnd}></Route>
+              <Route path='/rt-job' exact component={ReactTableJob}></Route>
+              <Route path='/js-saga' exact component={JobStyleSaga}></Route>
+              <Route path='/svg' exact component={SVG}></Route>
+              <Route path='/react-slick' exact component={ReactSlick}></Route>
+              <Route path='/qrcode' exact component={QrCode}></Route>
+              <Route path='/print' exact component={Print}></Route>
+              <Route path='/print-modal' exact component={PrintModal}></Route>
+              <Route path='/react-table-pagination' exact component={ReactTablePagination}></Route>
+              <Route path='/hooks/useState' exact component={UseState}></Route>
+              <Route path='/hooks/useRef' exact component={Ref}></Route>
+              <Route path='/hooks/useEffect/Demo1' exact component={UseEffectDemo1}></Route>
+              <Route path='/hooks/useEffect/Demo2' exact component={UseEffectDemo2}></Route>
+              <Route path='/formik-submit' exact component={FromikSubmit}></Route>
+              <Route path='/interview' exact component={Interview}></Route>
+              <Route path='/bs-col-table' exact component={BsColTable}></Route>
+              <Route path='/hooks/useMemo' exact component={UseMemo}></Route>
+              <Route path='/scroll-timeline' exact component={scrollTimeline}></Route>
+              <Route path='/mixBlendMode' exact component={mixBlendMode}></Route>
+              <Route path='/clip-path' exact component={ClipPath}></Route>
+              <Route path='/custom-hook' exact component={CustomHook}></Route>
+              <Route path='/bind-js' exact component={JsBind}></Route>
+              <Route path='/theme-provider' exact component={ThemeProvider}></Route>
+              <Route path='/form-test' exact component={FormTest}></Route>
+              <Route path='/phone-charging' exact component={PhoneCharging}></Route>
+              <Route path='/react-hook-form' exact component={ReactHookForm}></Route>
+              <Route path='/ref' exact component={theRef}></Route>
+            </Switch>
+          </Suspense>
         </Router>
       </IntlProvider>
     </div>
