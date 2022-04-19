8287535d1a9ca5290e44ba85320a8d3f6786022f
janlin002 <123davidbill@gmail.com>
2022-04-19 20:40:50 +0800

    props test

--- src/Demo/FrostedGlass/index.js
+++ src/Demo/FrostedGlass/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import './style.css'
 
+const Test= (values)=>{
+  // console.log(values, 'values')
+  const { name, age } = values
+  console.log(name, age)
+  return (
+    <div>123</div>
+  )
+}
+
 const FrostedGlass = () =>{
+  const props = {
+    name: 'david',
+    age: '12'
+  }
   return (
     <div>
       <div className="card">123</div>
       <div className="backdrop-blur">456</div>
       {/* not working */}
+      <Test 
+        {...props}
+      />
     </div>
   )
 }
