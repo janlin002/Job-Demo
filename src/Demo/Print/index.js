62864d04be8eb2939497e3c1a23e624eda8e7eed
janlin002 <123davidbill@gmail.com>
2022-05-24 20:47:01 +0800

    image

--- src/Demo/Print/index.js
+++ src/Demo/Print/index.js
@@ -4,6 +4,8 @@ import photo from '../../assets/image/photo1.png'
 const Print = () =>{
   const [photoInfo, setPhotoInfo] = useState('')
   const [photoBlob, setPhotoBlob] = useState({})
+
+  console.log(photoBlob, 'photoBlob')
   const handlePrintClick = ()=>{
     window.print()
   }
@@ -28,6 +30,28 @@ const Print = () =>{
     setPhotoBlob(blob)
   }
 
+  function BlobToBase64(blob, callback){
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onload = function (e) {
+      callback(e.target.result)
+    };
+  }
+
+  function preView(){
+    let reader    = new FileReader();
+
+    // getImageBlob( url , function(blob){
+    //   reader.readAsDataURL(blob);
+    // });
+
+    reader.onload = function() {
+      var img = document.createElement("img");
+      img.src = photoBlob;
+      document.body.appendChild(img);
+    }
+  }
+
   return (
     <>
       <button onClick={()=>handlePrintClick()}>列印</button>
@@ -46,6 +70,9 @@ const Print = () =>{
         <label className='custom-file-label' htmlFor='fileName'>
           選取檔案
         </label>
+
+        <button type="button" onClick={preView()}>預覽</button>
+        <img id="img" src="" alt="" />
         
         {/* <a href={photoInfo} download> */}
         <button onClick={()=>window.print(photoBlob)}>列印圖片</button>
