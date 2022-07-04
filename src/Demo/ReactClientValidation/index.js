9a3bf316c80489186d872c14cc6093b117f06efc
janlin002 <123davidbill@gmail.com>
2022-07-04 14:10:12 +0800

    add avn

--- src/Demo/ReactClientValidation/index.js
+++ src/Demo/ReactClientValidation/index.js
@@ -2,27 +2,27 @@ import React from 'react'
 // import handleValidation from 'react-client-validation'
 
 const ReactClientValidation = () => {
-//   const loginValidation = [
-//     {
-//       index: 'username',
-//       condition: [!username],
-//       errorMessage: 'User name is not valid!'
-//     },
-//     {
-//       index: 'password',
-//       errorFormat: ['invalidPassword'],
-//       customCondition: (data, errorReturnArray) => {
-//         if (data.password) {
-//           errorReturnArray.push(false)
-//         }
-//       },
-//     },
-//   ];
+  // const loginValidation = [
+  // {
+  //   index: 'username',
+  //   condition: [!username],
+  //   errorMessage: 'User name is not valid!'
+  // },
+  //   {
+  //     index: 'password',
+  //     errorFormat: ['invalidPassword'],
+  //     customCondition: (data, errorReturnArray) => {
+  //       if (data.password) {
+  //         errorReturnArray.push(false)
+  //       }
+  //     },
+  //   },
+  // ];
   
-  //   const [isPass, loginValidationError] = handleValidation({
-  //     errorArray: loginValidation,
-  //     defaultErrorMessage: "input can't be blank",
-  //   });
+  // const [isPass, loginValidationError] = handleValidation({
+  //   errorArray: loginValidation,
+  //   defaultErrorMessage: "input can't be blank",
+  // });
   return (
     <div>index</div>
   )
