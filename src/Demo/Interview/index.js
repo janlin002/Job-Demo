51c73011b71793ad1d9c67f53aa6b6dd5d916f99
janlin002 <123davidbill@gmail.com>
2022-08-03 11:20:58 +0800

    add linePay

--- src/Demo/Interview/index.js
+++ src/Demo/Interview/index.js
@@ -54,18 +54,50 @@ import React, { useState, useEffect } from 'react'
 
 // Answer: 123451245112
 
-function A () {
-  const [count, setCount] = useState(4);
+// function A () {
+//   const [count, setCount] = useState(4);
 
-  useEffect(()=>{
-    setCount(prev => prev + 1);
-    setCount(prev => prev + 1);
-    setCount(prev => prev + 1);
-  }, [])
+//   useEffect(()=>{
+//     setCount(prev => prev + 1);
+//     setCount(prev => prev + 1);
+//     setCount(prev => prev + 1);
+//   }, [])
 
-  console.log('A: ', count)
+//   console.log('A: ', count)
 
-  return null;
+//   return null;
+// }
+
+// export default A
+function ScopeTest(a) {
+  var b = 4;
+  function bar() {
+    console.log(`a:${a},b:${b},c:${c}`); // "a:3,b:4,c:5"
+  }
+  let c = 5;
+  console.log(a); // 3
+  console.log(b); // 4
+  console.log(c); // 5
+  bar();
 }
 
-export default A
+ScopeTest(3);
+
+export default ScopeTest()
+// console.log(a);
+
+// function showUserId() {
+//   // function scope
+//   var user_id = '111'
+  
+//   if (true) {
+//     // block scope
+//     let user_id_2 = '999'
+//     const user_id_3 = '777'
+//     console.log(user_id_3) // 777
+//   }
+  
+//   console.log(user_id) // 111
+//   console.log(user_id_2) // ReferenceError
+// }
+// showUserId()
---
+++ src/LinePay/README.md
@@ -0,0 +1,7 @@
+使用[express-generator](https://expressjs.com/zh-tw/starter/generator.html)，快速開啟專案
+
+使用下列指令來執行應用程式：
+
+```js
+DEBUG=linepay:* npm start
+```
