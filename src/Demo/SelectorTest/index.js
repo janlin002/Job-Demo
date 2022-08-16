6b710355a5bc01f182ae9a639750915f9ee9e4fb
janlin002 <123davidbill@gmail.com>
2022-08-16 20:12:43 +0800

    arrat reverse

--- src/Demo/SelectorTest/index.js
+++ src/Demo/SelectorTest/index.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { checkSagaData } from '../../Redux/selector'
 
 const SelectorTest = () =>{
+  // Push
   var arr = [1, 2, 3, 4, 5]
   //给push封装，向数组尾部追加值
   function push() {
@@ -14,6 +15,19 @@ const SelectorTest = () =>{
     console.log(arr);
   }
   push(6, 7)//[1,2,3,4,5,6,7]
+
+  // Reverse
+  const arr2 = [10, 5, 3, 8, 2] // 2,8,3,5,10
+
+  function reverse(arr2){
+    const arr3 = [...arr2]
+    for(let i = 0; arr2.length > i; i++){
+      arr2[i] = arr3[arr3.length - 1 - i]
+    }
+    console.log(arr2)
+  }
+
+  reverse(arr2)
 
   return (
     <div>123</div>
