dd68ba348605b05bb2acb2ea86cf8379ebcb5bee
janlin002 <123davidbill@gmail.com>
2022-08-23 16:21:05 +0800

    delete avn && create splice, slice, split handWrite

--- src/Demo/handWrite/index.js
+++ src/Demo/handWrite/index.js
@@ -87,17 +87,15 @@ const Index = () => {
   const arr9 = [10, 5, 3, 8, 2]
 
   function reduce(arr){
-    console.log(arr, 'arr')
     const len = arr.length
-    let red = 0
+    let reducer = 0
     for(let i = 0; len > i; i++){
-      red += arr[i]
+      reducer += arr[i]
     }
-    return red
+    return reducer
   }
 
   const toReduce = reduce(arr9)
-  console.log(toReduce, 'toReduce')
 
   // sort
 
@@ -120,11 +118,77 @@ const Index = () => {
 
   const toSort1 = bSort(arr8)
 
-  // slice
-
   // splice
+  let arr10 = ['first', 'second', 'third']
 
-  // split
+  Array.prototype.mySplice = function (...arg) {
+    let index = arg[0]//準備刪除的位置 => 2
+    let count = arg[1]//刪除的個數 => 1
+    let add_arr = [] //添加的元素數組
+    let delIndex_arr = [] //刪除數組的索引
+    let _return_arr = []//返回的數組
+
+    for (let i = index; i < index + count; i++) {
+      console.log(i, 'i')
+      delIndex_arr.push(i)
+    }
+    let this_finishArr = [] //存放處理之後的數組
+    // 把索引之前的原數組成員全放到新數組裏去
+    this.forEach((im, idx) => {
+      if (idx < index) {
+        this_finishArr.push(im)
+      }
+    })
+    // 處理返回值
+    delIndex_arr.forEach((im) => {
+      _return_arr.push(this[im])
+    })
+    // 把用戶輸入需要添加的元素放到新數組裏去
+    for (let i = 2; i < arg.length; i++) {
+      this_finishArr.push(arg[i])
+    }
+    // 把添加之後，原數組的剩餘成員，全部添加到新數組
+    for (let i = index + count; i < this.length; i++) {
+      this_finishArr.push(this[i])
+    }
+    // 將新數組深拷貝給原數組
+    this.length = this_finishArr.length
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this_finishArr[i]
+    }
+    return _return_arr
+
+  }
+  // console.log(arr10.mySplice(2, 1)) //[third]
+  // console.log(arr10) //[first,second]
+
+  // slice
+  const arr11 = [1, 2, 3, 4, 5, 6]
+  Array.prototype.slice = function (start, end) {
+    // 保存结果
+    let res = [];
+    // this指向调用slice方法的数组
+    let len = this.length;
+
+    // 確定有傳值
+    start = start === undefined ? 0 : start;
+    end = end === undefined ? len : end;
+
+    start = start < 0 ? start + len : start;
+    start = Math.max(0, start); // 算取最大值
+
+    end = end < 0 ? end + len : end;
+    end = Math.min(end, len) // 算取最小值
+    
+    // 截取[start,end)这个区间的元素
+    for (let i = start; i < end; i++) {
+      res.push(this[i])
+    }
+    return res;
+  }
+
+  const test = arr11.slice(0, 5)
+
   //Debounce
   // function debounce(func, wait) {
   //   var timeout;
@@ -195,7 +259,7 @@ const Index = () => {
   // }
   // let timerID = setTimeout(showName, 1000);
 
-  function mysetTimeout(fn, time){
+  function setTimeout(fn, time){
     let now = Date.now();
     let flag = true;
     while(flag){
@@ -209,7 +273,7 @@ const Index = () => {
   // let test = mysetTimeout(showName, 1000)
   // test
 
-  function mysetInterval(fn, time){
+  function setInterval(fn, time){
     let timeId = null;
     let isClear = false;
     function interval(){
