afbf05ff3f3a66c8e6627e116ecd0729fa57146d
janlin002 <123davidbill@gmail.com>
2022-09-01 17:57:02 +0800

    adjust header list

---
+++ src/assets/Data/HeaderList.js
@@ -0,0 +1,216 @@
+const HeaderList = [
+  {
+    to: 'formik',
+    name: 'Formik'
+  },
+  {
+    to: 'formikyup',
+    name: 'FormikYup'
+  },
+  {
+    to: 'reacttable',
+    name: 'React-table'
+  },
+  {
+    to: 'reactintl',
+    name: 'React-intl'
+  },
+  {
+    to: 'chart',
+    name: 'Chart'
+  },
+  {
+    to: 'colspan',
+    name: 'colspan'
+  },
+  {
+    to: 'saga',
+    name: 'saga'
+  },
+  {
+    to: 'jsx-control',
+    name: 'jsx-control'
+  },
+  {
+    to: 'day-js',
+    name: 'Day.Js'
+  },
+  {
+    to: 'formik-2',
+    name: 'formik 2.0'
+  },
+  {
+    to: 'react-modal',
+    name: 'React-modal'
+  },
+  {
+    to: 'selector-test',
+    name: 'array-method',
+  },
+  {
+    to: 'react-dnd',
+    name: 'React-dnd'
+  },
+  {
+    to: 'rt-job',
+    name: 'React-table-job'
+  },
+  {
+    to: 'js-saga',
+    name: 'job-style-saga'
+  },
+  {
+    to: 'svg',
+    name: 'svg'
+  },
+  {
+    to: 'react-slick',
+    name: 'react-slick'
+  },
+  {
+    to: 'qrcode',
+    name: 'react-qrcode'
+  },
+  {
+    to: 'print',
+    name: 'Print'
+  },
+  {
+    to: 'print-modal',
+    name: 'Print-Modal'
+  },
+  {
+    to: 'react-table-pagination',
+    name: 'ReactTablePagination'
+  },
+  {
+    to: '/hooks/useState',
+    name: 'useState'
+  },
+  {
+    to: '/hooks/useRef',
+    name: 'useRef'
+  },
+  {
+    to: '/hooks/useEffect/Demo1',
+    name: 'useEffect Demo1'
+  },
+  {
+    to: '/hooks/useEffect/Demo2',
+    name: 'useEffect Demo2'
+  },
+  {
+    to: 'formik-submit',
+    name: 'formik submit'
+  },
+  {
+    to: 'interview',
+    name: 'interview'
+  },
+  {
+    to: 'bs-col-table',
+    name: 'bs-col-table'
+  },
+  {
+    to: 'hooks/useMemo',
+    name: 'useMemo'
+  },
+  {
+    to: 'scroll-timeline',
+    name: 'scrollTimeline'
+  },
+  {
+    to: 'mixBlendMode',
+    name: 'mixBlendMode'
+  },
+  {
+    to: 'clip-path',
+    name: 'ClipPath'
+  },
+  {
+    to: 'custom-hook',
+    name:'CustomHook'
+  },
+  {
+    to: 'bind-js',
+    name: 'JsBind'
+  },
+  {
+    to: 'theme-provider',
+    name: 'ThemeProvider'
+  },
+  {
+    to: 'form-test',
+    name: 'form-test'
+  },
+  {
+    to: 'phone-charging',
+    name: 'phone-charging'
+  },
+  {
+    to: 'react-hook-form',
+    name: 'react-hook-form' 
+  },
+  {
+    to: 'ref',
+    name: 'ref'
+  },
+  {
+    to: 'frosted-glass',
+    name: 'frosted-glass' 
+  },
+  {
+    to: 'react-query',
+    name: 'react-query'
+  },
+  {
+    to: 'immer',
+    name:'immer'
+  },
+  {
+    to: 'redux-toolkit',
+    name: 'redux-toolkit'
+  },
+  {
+    to: 'drive-js',
+    name: 'drive-js'
+  },
+  {
+    to: 'react-plx',
+    name: 'react-plx'
+  },
+  {
+    to: 'js-doc',
+    name: 'js-doc'
+  },
+  {
+    to: 'react-loading-skeleton',
+    name: 'react-loading-skeleton'
+  },
+  {
+    to: 'google-oauth',
+    name: 'google-oauth'
+  },
+  {
+    to: 'react-router',
+    name:'react-router'
+  },
+  {
+    to: 'react-client-validation',
+    name: 'react-client-validation'
+  },
+  {
+    to: 'swiper',
+    name: 'swiper'
+  },
+  {
+    to: 'hand-write',
+    name: 'hand-write',
+  },
+  {
+    to: 'react-check',
+    name: 'react-check'
+  }
+]
+
+export default HeaderList
