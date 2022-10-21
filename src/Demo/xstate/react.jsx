91777bd8e5554cf6f5a1a15a6f0863c25549309f
janlin002 <123davidbill@gmail.com>
2022-10-21 10:42:11 +0800

    [FIX] fix travis CI error

--- src/Demo/xstate/react.jsx
+++ src/Demo/xstate/react.jsx
@@ -45,22 +45,22 @@ promiseService.start()
 promiseService.send({ type: 'RESOLVE' })
 // => 'resolved'
 
-// const ReactXstate = () => {
-//   const [state, send] = useMachine(promiseMachine)
+const ReactXstate = () => {
+  const [state, send] = useMachine(promiseMachine)
 
-//   return (
-//     <div>
-//       {/** You can listen to what state the service is in */}
-//       {state.matches('pending') && <p>Loading...</p>}
-//       {state.matches('rejected') && <p>Promise Rejected</p>}
-//       {state.matches('resolved') && <p>Promise Resolved</p>}
-//       <div>
-//         {/** You can send events to the running service */}
-//         <button onClick={() => send('RESOLVE')}>Resolve</button>
-//         <button onClick={() => send('REJECT')}>Reject</button>
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      {/** You can listen to what state the service is in */}
+      {state.matches('pending') && <p>Loading...</p>}
+      {state.matches('rejected') && <p>Promise Rejected</p>}
+      {state.matches('resolved') && <p>Promise Resolved</p>}
+      <div>
+        {/** You can send events to the running service */}
+        <button onClick={() => send('RESOLVE')}>Resolve</button>
+        <button onClick={() => send('REJECT')}>Reject</button>
+      </div>
+    </div>
+  )
+}
 
-// export default ReactXstate
+export default ReactXstate
