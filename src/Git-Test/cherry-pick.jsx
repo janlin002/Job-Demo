8b1cf56c55746c89300b62bf45b0919813fc7d8a
janlin002 <123davidbill@gmail.com>
2022-11-19 19:46:46 +0800

    cherry-pick-test

---
+++ src/Git-Test/cherry-pick.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CherryPick = () => {
+  return (
+    <>
+      <div>cherry-pick</div>
+      <div>cherry-pick-2</div>
+      <div>cherry-pick-3</div>
+    </>
+  )
+}
+
+export default CherryPick
