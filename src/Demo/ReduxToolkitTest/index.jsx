4b97462a60699ca6a9b07f4ed0e36447249dc0ab
janlin002 <123davidbill@gmail.com>
2023-10-15 20:12:19 +0800

    feat: add addMatcher

--- src/Demo/ReduxToolkitTest/index.jsx
+++ src/Demo/ReduxToolkitTest/index.jsx
@@ -1,64 +1,51 @@
-import React, { useState } from 'react'
-import {
-  useDispatch,
-  useSelector
-} from 'react-redux'
-import {
-  selectTodo,
-  addTodo
-} from '../../slice/todo'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTodo, addTodo } from "../../slice/todo";
 import {
   useGetTodoQuery,
   useAddTodoMutation,
-  todoApiSlice } from '../../ReduxToolkitApiSlice/todoApiSlice'
+  todoApiSlice,
+} from "../../ReduxToolkitApiSlice/todoApiSlice";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 
-const ReduxToolkitTest = () =>{
-  const dispatch = useDispatch()
-  const [newTodo, setNewTodo] = useState('')
-  const states = useSelector(selectTodo)
+const ReduxToolkitTest = () => {
+  const dispatch = useDispatch();
+  const [newTodo, setNewTodo] = useState("");
+  const states = useSelector(selectTodo);
 
-  const check = useSelector(state => state)
+  const check = useSelector((state) => state);
 
-  console.log(check, 'check')
+  console.log(check, "check");
 
-  console.log(todoApiSlice.endpoints.getTodo.useQueryState().data, 'todoApiSlice')
+  // console.log(
+  //   todoApiSlice.endpoints.getTodo.useQueryState().data,
+  //   "todoApiSlice"
+  // );
 
-  // console.log(check.todoApi.queries.getTodo(undefined).data, 'check')
+  // console.log(check.todoApi.queries.getTodo(undefined).data, "check");
 
-  const handleChangeRedux = () =>{
-    dispatch(
-      addTodo({ id: 3, name: "a new todo item" })
-    )
-  }
+  const handleChangeRedux = () => {
+    dispatch(addTodo({ id: 3, name: "a new todo item" }));
+  };
 
-  const {
-    data,
-    isLoading,
-    isSuccess,
-    isError,
-    error
-  } = useGetTodoQuery()
-  const [addTodo, addTodoResponse] = useAddTodoMutation()
+  const { data, isLoading, isSuccess, isError, error } = useGetTodoQuery();
+  const [addTodo, addTodoResponse] = useAddTodoMutation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodo({ userId: 1, title: newTodo, completed: false })
-    setNewTodo('')
-  }
+    addTodo({ userId: 1, title: newTodo, completed: false });
+    setNewTodo("");
+  };
 
-  console.log(data, 'data')
+  console.log(data, "data");
 
   return (
     <ul>
-      <button 
-        type="button"
-        onClick={handleChangeRedux}
-      >
+      <button type="button" onClick={handleChangeRedux}>
         點擊更改
       </button>
-      
+
       <form onSubmit={handleSubmit}>
         <label htmlFor="new-todo">Enter a new todo item</label>
         <div className="new-todo">
@@ -75,34 +62,32 @@ const ReduxToolkitTest = () =>{
           點擊
         </button>
       </form>
-      {
-        isSuccess && (
-          data.map(todo => { //JSON.stringify(todos)
-            return (
-              <article key={todo.id}>
-                <div className="todo">
-                  <input
-                    type="checkbox"
-                    checked={todo.completed}
-                    id={todo.id}
-                    // onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
-                  />
-                  <label htmlFor={todo.id}>{todo.title}</label>
-                </div>
-                <button 
-                  className="trash" 
-                  // onClick={() => deleteTodo({ id: todo.id })}
-                >
-                  {/* <FontAwesomeIcon icon={faTrash} /> */}
-                  點擊
-                </button>
-              </article>
-            )
-          })
-        )
-      }
+      {isSuccess &&
+        data.map((todo) => {
+          //JSON.stringify(todos)
+          return (
+            <article key={todo.id}>
+              <div className="todo">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  id={todo.id}
+                  // onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+                />
+                <label htmlFor={todo.id}>{todo.title}</label>
+              </div>
+              <button
+                className="trash"
+                // onClick={() => deleteTodo({ id: todo.id })}
+              >
+                {/* <FontAwesomeIcon icon={faTrash} /> */}
+                點擊
+              </button>
+            </article>
+          );
+        })}
     </ul>
-  )
-}
+  );
+};
 
-export default ReduxToolkitTest
+export default ReduxToolkitTest;
--- src/slice/todo/index.js
+++ src/slice/todo/index.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+import { todoApiSlice } from "../../ReduxToolkitApiSlice/todoApiSlice";
+
 export const todoSlice = createSlice({
   name: "todo",
   initialState: {
@@ -6,17 +8,27 @@ export const todoSlice = createSlice({
       { id: 1, name: "first todo on redux" },
       { id: 2, name: "second todo in list" },
     ],
+    todoTest: "",
   },
   reducers: {
     addTodo: (state, action) => {
-      state.todolist.push(action.payload)
+      state.todolist.push(action.payload);
     },
   },
-})
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      todoApiSlice.endpoints.getTodo.matchFulfilled,
+      (state, action) => {
+        console.log(action, "action");
+        state.todoTest = action.payload;
+      }
+    );
+  },
+});
 
-export const selectTodo = (state) => state.todo
-export const { addTodo } = todoSlice.actions
-export default todoSlice.reducer
+export const selectTodo = (state) => state.todo;
+export const { addTodo } = todoSlice.actions;
+export default todoSlice.reducer;
 
 // initialState：就是放所有狀態初始值
 // reducers：通常是放很多的函式，會有兩個參數state和action
